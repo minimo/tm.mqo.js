097ca780a337878abab0e51a8d8cd7912f2e9735
minimo <minimo.mobile@gmail.com>
2015-07-13 16:27:43 +0900

    change regexp

--- tm.mqo.js
+++ tm.mqo.js
@@ -392,7 +392,7 @@
 
         //マテリアル情報のパース
         parse: function(data) {
-            var infoText    = data.match(/^Material [0-9]* \{\r\n([\s\S]*?)\n^\}$/m);
+            var infoText    = data.match(/^Material [0-9]* \{\r\n([\s\S]*?)\n^\}$/gmi);
             var matTextList = infoText[1].split('\n');
 
             for (var i = 0, len = matTextList.length; i < len; i++) {
