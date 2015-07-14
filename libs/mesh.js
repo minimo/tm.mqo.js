9aff6bf69315bf150b23591fa37102d35af73904
minimo <minimo.mobile@gmail.com>
2015-07-14 16:17:56 +0900

    add multi mesh

--- libs/mesh.js
+++ libs/mesh.js
@@ -19,6 +19,9 @@
                         this.superInit(asset.mesh.clone());
                     } else if (asset instanceof tm.asset.MQO) {
                         this.superInit(asset.model.meshes[0]);
+                        for (var i = 1; i < asset.model.meshes.length; i++) {
+                            tm.hybrid.Mesh(asset.model.meshes[i]).addChildTo(this);
+                        }
                     }
                 } else {
                     console.error("アセット'{0}'がないよ".format(mesh));
